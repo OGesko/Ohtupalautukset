8909dcbd73d6d3c717428fd9f439e475867c3548
OGesko <eskelinenvilkko@gmail.com>
2024-11-05 14:49:49 +0200

    tehtävä 2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,27 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, prod_license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = prod_license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
     def _stringify_dependencies(self, dependencies):
         return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+    
+    def list_results(self, list):
+        text = ""
+        for i in list:
+            text += (f"\n - {i}")
+        return text
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense:: {self.license or '-'}"
+            f"\n\nAuthors: {self.list_results(self.authors)}"
+            f"\n\nDependencies: {self.list_results(self.dependencies)}"
+            f"\n\nDevelopment dependencies: {self.list_results(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import toml
 
 
 class ProjectReader:
@@ -9,7 +10,16 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        #print(content)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        data = toml.loads(content)
+        #print(data)
+        udata = data.get("tool").get("poetry")
+        name = udata.get("name")
+        description = udata.get("description")
+        prod_license = udata.get("license")
+        authors = udata.get("authors")
+        dependencies = udata.get("dependencies").keys()
+        dev_dependencies = udata.get("group").get("dev").get("dependencies").keys()
+        return Project(name, description, prod_license, authors, dependencies, dev_dependencies )
