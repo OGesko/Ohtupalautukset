e50e66d681524beea84810ce6afde0bf9dc22ec9
OGesko <eskelinenvilkko@gmail.com>
2024-11-10 18:49:28 +0200

    teht12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,14 @@
-from player import Player
+import requests
+from player import Player, PlayerReader, PlayerStats
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,37 @@
+import requests
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.nationality = dict['nationality']
+
+    def points(self):
+        return self.goals + self.assists
+
     def __str__(self):
-        return self.name
+        return f"{self.name:20}{self.team:4} {self.goals:2} + {self.assists:2} = {self.points()}"
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def players(self):
+        response = requests.get(self.url).json()
+        players = []
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+        return players
+    
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.players()
+
+    def top_scorers_by_nationality(self, nationality):
+        fPlayers = []
+        for p in self.players:
+            if p.nationality == nationality:
+                fPlayers.append(p)
+        return sorted(fPlayers, key=lambda p: p.points(), reverse=True)
